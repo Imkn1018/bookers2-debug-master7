5e07b03249f7615b106cb048002101dfd09af9c8
Imkn1018 <o2oneru2505@gmail.com>
2021-04-16 07:47:46 +0000

    [finish] path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ログイン、ログアウト後の遷移先
+  def after_sign_in_path_for(resource)
+    user_path(@user)
+  end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
