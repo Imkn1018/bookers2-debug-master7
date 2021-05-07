64d2954eb414bd572cf9390ded9bf8d460db4793
EC2 Default User <ec2-user@ip-172-31-7-233.ec2.internal>
2021-05-07 08:06:15 +0000

    [Finish]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,16 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:user_name, :email, :password, :password_confirmation, :remember_me]
+    added_attrs = [
+    :user_name, 
+    :email,:postcode,
+    :prefecture_name,
+    :address_city,
+    :address_street, 
+    :password, 
+    :password_confirmation, 
+    :remember_me
+    ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ApplicationRecord
   validates :name, uniqueness: true, length: {minimum: 2, maximum: 20}
   validates :introduction, length:{maximum: 50}
   attachment :profile_image
+  
+  # 都道府県コードから都道府県名に自動で変換する。
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  # ~.prefecture_nameで都道府県名を参照出来る様にする。
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 end
